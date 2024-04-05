d652f65e144dd8d4836b33f5e08afe814f2dde08
Ayush Singh <ayush6203@gmail.com>
2024-04-05 06:26:12 +0530

    Trie implementation

---
+++ LLDProblems/DataStructuresDesign/Trie.cs
@@ -0,0 +1,59 @@
+﻿namespace LLDProblems.DataStructuresDesign
+{
+    public class Trie
+    {
+        TrieNode _root;
+        public Trie()
+        {
+            _root = new TrieNode();
+        }
+
+        public void Insert(string word)
+        {
+            TrieNode currNode = _root;
+            foreach(char c in word)
+            {
+                if (currNode.Child[c - 'a'] is null)
+                    currNode.Child[c - 'a'] = new TrieNode();
+
+                currNode = currNode.Child[c - 'a'];
+            }
+
+            currNode.IsEndOfWord = true;
+        }
+
+        public bool Search(string word)
+        {
+            TrieNode currNode = _root;
+            foreach (char c in word)
+            {
+                if (currNode.Child[c - 'a'] is null)
+                    return false;
+
+                currNode = currNode.Child[c - 'a'];
+            }
+
+            return currNode.IsEndOfWord;
+        }
+
+        public bool Prefix(string word)
+        {
+            TrieNode currNode = _root;
+            foreach (char c in word)
+            {
+                if (currNode.Child[c - 'a'] is null)
+                    return false;
+
+                currNode = currNode.Child[c - 'a'];
+            }
+
+            return true;
+        }
+
+        class TrieNode
+        {
+            public bool IsEndOfWord = false;
+            public TrieNode[] Child = new TrieNode[26];
+        }
+    }
+}
--- LLDProblems/Program.cs
+++ LLDProblems/Program.cs
@@ -15,15 +15,31 @@ Console.WriteLine("Here you will find all LLD problems");
 
 
 
-Console.WriteLine("Here you can try Data Structures");
-LRU lru = new LRU(5);
-lru.AddKeyValue("ayush", "ayush is good");
-lru.AddKeyValue("piyush", "piyush is good");
-lru.AddKeyValue("saroj", "saroj is good");
-lru.AddKeyValue("arsh", "arsh is good");
-lru.AddKeyValue("siya", "siya is good");
-Console.WriteLine(lru.GetValue("ayush"));
-lru.AddKeyValue("babi", "babi is good");
-Console.WriteLine(lru.GetValue("saroj"));
-lru.AddKeyValue("laliya", "laliya is good");
-Console.WriteLine(lru.GetValue("ayush"));
+# region Data Structures Design
+
+// LRU Desing
+//LRU lru = new LRU(5);
+//lru.AddKeyValue("ayush", "ayush is good");
+//lru.AddKeyValue("piyush", "piyush is good");
+//lru.AddKeyValue("saroj", "saroj is good");
+//lru.AddKeyValue("arsh", "arsh is good");
+//lru.AddKeyValue("siya", "siya is good");
+//Console.WriteLine(lru.GetValue("ayush"));
+//lru.AddKeyValue("babi", "babi is good");
+//Console.WriteLine(lru.GetValue("saroj"));
+//lru.AddKeyValue("laliya", "laliya is good");
+//Console.WriteLine(lru.GetValue("ayush"));
+
+
+// Trie Design
+Trie trie = new Trie();
+trie.Insert("a");
+trie.Insert("ab");
+trie.Insert("abc");
+trie.Insert("apple");
+trie.Insert("applea");
+trie.Search("apple");
+trie.Search("applea");
+
+
+#endregion
